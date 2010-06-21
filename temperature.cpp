daa340129f176abc64f5307de210b9defef62dde
Marek Belisko <marek.belisko@gmail.com>
2010-06-21 13:03:56 +0200

    Adapted according actual libiqrf changes.
    Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

--- temperature.cpp
+++ temperature.cpp
@@ -8,7 +8,7 @@ temperature::temperature()
 
 temperature::~temperature()
 {
-    release_device();
+    iqrf_release_device();
 }
 
 int temperature::write_read_spi_data(unsigned char *data, int data_len, int write_read)
@@ -18,17 +18,17 @@ int temperature::write_read_spi_data(unsigned char *data, int data_len, int writ
     if (data_len > 0x35)
         data_len = 0x35;
 
-    ret_val = read_write_spi_cmd_data(data, data_len, write_read);
+    ret_val = iqrf_read_write_spi_cmd_data(data, data_len, write_read);
 
     return ret_val;
 }
 
 int temperature::init()
 {
-    return init_device();
+    return iqrf_init_device();
 }
 
 int temperature::get_status()
 {
-    return get_spi_status();
+    return iqrf_get_spi_status();
 }
